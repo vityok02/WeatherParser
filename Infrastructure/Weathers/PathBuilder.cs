e84604e2f21aba4d328d25ed55661292bce3f5d7
vityok02 <lipkan02@gmail.com>
2024-04-11 23:48:04 +0300

    add weather api

---
+++ Infrastructure/Weathers/PathBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Weathers;
+
+public static class PathBuilder
+{
+    public static string BuildPath(
+        string basePath,
+        string parameter,
+        string token)
+    {
+        return $"{basePath}?q={parameter}&key={token}";
+    }
+}
---
+++ Infrastructure/Weathers/WeatherClientConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Weathers;
+
+public record WeatherClientConfiguration
+{
+    public static readonly string Configuration = "WeatherConfiguration";
+    public string Path { get; init; } = "";
+    public string Parametes { get; init; } = "";
+}
