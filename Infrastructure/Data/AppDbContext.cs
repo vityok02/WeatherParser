d384fb306354b0047c4d165b37d78a3c788b6d7c
Віктор Липкань <lipkan02@gmail.com>
2024-07-30 19:40:58 +0300

    add docker

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -14,6 +14,7 @@ public class AppDbContext : DbContext, IAppDbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
+        Database.EnsureCreated();
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -14,7 +14,21 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("localDb");
+        var connectionString = string.Empty;
+        bool useDocker = false;
+
+        var v = Environment.GetEnvironmentVariable("UseDocker");
+
+        _ = bool.TryParse(Environment.GetEnvironmentVariable("UseDocker"), out useDocker);
+
+        if (useDocker)
+        {
+            connectionString = "Server=bot-db,1433;Database=TgBot;User Id=sa;Password=yourStrong(!)Password; TrustServerCertificate=true";
+        }
+        else
+        {
+            connectionString = configuration.GetConnectionString("localDb");
+        }
 
         services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(connectionString));
