fe73c08f5a641e3647a3a122174e95d8f5753ea3
lipkan02 <lipkan02@gmail.com>
2025-12-30 22:19:09 +0200

    Refactor DI setup and remove EF migration history
    Centralize BotConfiguration and Telegram client registration in DI extension. Update Program.cs to use new DI pattern and switch DB init to EnsureCreated. Remove existing EF Core migration files to reset migration history.

--- Bot/DependencyInjection.cs
+++ Bot/DependencyInjection.cs
@@ -4,17 +4,33 @@ using Application.Common.Interfaces.Messaging;
 using Application.Common.Interfaces.ReplyMarkup;
 using Application.Keyboard;
 using Bot.BotHandlers;
+using Bot.Configurations;
+using Bot.Extensions;
 using Bot.Messages;
 using Bot.Services;
 using Bot.TgTypes;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace Bot;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    public static IServiceCollection AddPresentation(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
+        services.Configure<BotConfiguration>(
+            configuration.GetSection(BotConfiguration.Configuration));
+
+        services.AddHttpClient("telegram_bot_client")
+            .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+            {
+                BotConfiguration? botConfig = sp.GetConfiguration<BotConfiguration>();
+                TelegramBotClientOptions options = new(botConfig.BotToken);
+                return new TelegramBotClient(options, httpClient);
+            });
+
         services
             .AddScoped<ReceiverService>()
             .AddHostedService<PollingService>()
@@ -26,6 +42,7 @@ public static class DependencyInjection
             .AddScoped<IKeyboardMarkupGenerator, KeyboardMarkupGenerator>()
             .AddScoped<IRemoveKeyboardMarkup, RemoveKeyboardMarkup>()
             .AddScoped<IDefaultKeyboardFactory, DefaultKeyboardFactory>()
+
             ;
 
         return services;
